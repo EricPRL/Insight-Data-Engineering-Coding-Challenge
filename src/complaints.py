e0734094dcf64f8e0d06b34cc2e10fdb9f6abcda
EricPRL <55366873+EricPRL@users.noreply.github.com>
2020-07-28 19:47:59 -0700

    Update complaints.py

--- src/complaints.py
+++ src/complaints.py
@@ -1,7 +1,22 @@
+#!/user/bin/env python3
+
+## 
+## Peiran Li
+## 07/27/2020
+##
+## For one product and a certain year, aquire number of complaints, number of companies
+## and highest percentage of total complaints against one single company.
+
+## To handle unexpectd data in a large amount of dataset
+## when we read line by line, we print error messgaes
+## including line number instead of raising error.
+## This way could help us to find where the error is and fix data.
+## We use edge cases to test the code if it passes rare cases.
+##
+
 import sys
 import csv
 from collections import Counter
-
 
 class solution:
     
@@ -62,10 +77,7 @@ class solution:
             # Put the list together
             output.append([product,year,str(total_number),str(companies),str(result)])
         
-        return output
-    
-
-        
+        return output     
         
 def get_record(filename):
 
